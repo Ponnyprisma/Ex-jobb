ed8d60afdf12b8dc9beeac79d2e1c75627b2b83a
Dina Sandberg <dinasandberg@Dinas-MacBook-Pro-2.local>
2017-01-04 13:48:19 +0100

    fonts added

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,17 +1,17 @@
 console.log('\'Allo \'Allo!');
 
-$('body').addClass('animation');
+
 
 
 $( document ).ready(function() {
 
 var artWorkSad = ['images/quiz.jpg', 'images/rednails.jpg', 'images/sea.jpg', 'images/winter.jpg', 'images/dust.jpg'];
-var random = Math.floor(Math.random() * artWorkSad.length);
+var randomSad = Math.floor(Math.random() * artWorkSad.length);
 
 for (var i = 0; i < artWorkSad.length; i++) {
 	
-	document.getElementById("art-work-sad").src = artWorkSad[random];
-	console.log(artWorkSad[random]);
+	document.getElementById('art-work-sad').src = artWorkSad[randomSad];
+	console.log(artWorkSad[randomSad]);
 
 }
 
@@ -20,52 +20,58 @@ var random = Math.floor(Math.random() * artWorkHappy.length);
 
 for (var i = 0; i < artWorkHappy.length; i++) {
 	
-	document.getElementById("art-work-happy").src = artWorkHappy[random];
+	document.getElementById('art-work-happy').src = artWorkHappy[random];
 	console.log(artWorkHappy[random]);
 
 }
 
 var artworkLinkHappy = document.createElement('a');
-	artworkLinkHappy.className = "download-btn-happy";
+	artworkLinkHappy.className = 'download-btn-happy';
 	artworkLinkHappy.href = artWorkHappy[random];
 	artworkLinkHappy.download = artWorkHappy[random];
-	artworkLinkHappy.textContent = 'Download you artwork!';
-	document.getElementById("download-btn-container-happy").appendChild(artworkLinkHappy); 
+	artworkLinkHappy.textContent = 'Download your happy artwork!';
+	document.getElementById('download-btn-container-happy').appendChild(artworkLinkHappy); 
 
 var artworkLinkSad = document.createElement('a');
-	artworkLinkSad.className = "download-btn-sad";
-	artworkLinkSad.href = artWorkSad[random];
-	artworkLinkSad.download = artWorkSad[random];
+	artworkLinkSad.className = 'download-btn-sad';
+	artworkLinkSad.href = artWorkSad[randomSad];
+	artworkLinkSad.download = artWorkSad[randomSad];
 	artworkLinkSad.textContent = 'Download your sad artwork!';
-	document.getElementById("download-btn-container-sad").appendChild(artworkLinkSad); 
+	document.getElementById('download-btn-container-sad').appendChild(artworkLinkSad); 
+
+
+
+	$('body').addClass('animation');
+/*
+	setTimeout(function(){
+  		$('.fly-in-text').removeClass('hide-text');
+  	}, 500);
+*/	 
 	
-
+//////////////////////////////HAPPY FLOW//////////////////////////////////////////////
 	
-
-
-// start sad btn flow
 	$('#sad-smiley-btn').on('click', function() {
 		$('#btn-feeling-one-sad').show();
 		$('#btn-feeling-two-sad').show();
-		$('.feeling-copy-section-1').show();
+		$('.feeling-copy-section-1').fadeIn(2000);
 		$('#happy-smiley-btn').hide();
 		$('#sad-smiley-btn').hide();
 		$('body').addClass('sadBackground');
-		$('.how-do-you-feel').hide();
+		$('.fly-in-text').hide();
+		$('#how-do-you-feel').hide();
+		$('.explanation-text').hide();
 	});
 
 	$('#btn-feeling-one-sad').on('click', function() {
 		$('.container-feeling-one-sad').show();
 		$('.button-section-1-sad').hide();
 
-		
 		  setTimeout(function() {
      		$('.container-feeling-one-sad').hide();
      		$('#btn-feeling-three-sad').show();   
      		$('#btn-feeling-four-sad').show();  
-     		$('.feeling-copy-section-2').show(); 
-    
-  		}, 1000);
+     		$('.feeling-copy-section-2').fadeIn(2000);
+  		}, 7000);
 	});
 
 
@@ -77,8 +83,8 @@ var artworkLinkSad = document.createElement('a');
      		$('.container-feeling-two-sad').hide();  
      		$('#btn-feeling-three-sad').show();   
      		$('#btn-feeling-four-sad').show();  
-     		$('.feeling-copy-section-2').show();  
-  		}, 1000);
+     		$('.feeling-copy-section-2').fadeIn(2000);
+  		}, 7000);
 	});
 
 	$('#btn-feeling-three-sad').on('click', function() {
@@ -89,8 +95,8 @@ var artworkLinkSad = document.createElement('a');
 			$('.container-feeling-three-sad').hide(); 
 			$('#btn-feeling-five-sad').show();   
      		$('#btn-feeling-six-sad').show();   
-     		$('.feeling-copy-section-3').show();
-		}, 1000);
+     		$('.feeling-copy-section-3').fadeIn(2000);
+		}, 7000);
 	});
 
 	$('#btn-feeling-four-sad').on('click', function() {
@@ -101,9 +107,8 @@ var artworkLinkSad = document.createElement('a');
 			$('.container-feeling-four-sad').hide();
 			$('#btn-feeling-five-sad').show();   
      		$('#btn-feeling-six-sad').show(); 
-     		$('.feeling-copy-section-3').show();
-		}, 1000);
-
+     		$('.feeling-copy-section-3').fadeIn(2000);
+		}, 7000);
 	});
 
 	$('#btn-feeling-five-sad').on('click', function() {
@@ -114,9 +119,8 @@ var artworkLinkSad = document.createElement('a');
 			$('.container-feeling-five-sad').hide();
 			$('#btn-feeling-seven-sad').show();   
      		$('#btn-feeling-eight-sad').show(); 
-     		$('.feeling-copy-section-4').show();
-		}, 1000);
-
+     		$('.feeling-copy-section-4').fadeIn(2000);
+		}, 7000);
 	});
 
 	$('#btn-feeling-six-sad').on('click', function() {
@@ -127,9 +131,8 @@ var artworkLinkSad = document.createElement('a');
 			$('.container-feeling-six-sad').hide();
 			$('#btn-feeling-seven-sad').show();   
      		$('#btn-feeling-eight-sad').show(); 
-     		$('.feeling-copy-section-4').show();
-		}, 1000);
-
+     		$('.feeling-copy-section-4').fadeIn(2000);
+		}, 7000);
 	});
 
 	$('#btn-feeling-seven-sad').on('click', function() {
@@ -139,16 +142,15 @@ var artworkLinkSad = document.createElement('a');
 		setTimeout(function() {
 			$('.container-feeling-seven-sad').hide()
      		$('.container-loading-artwork').show();
+     		//$('.text-rendering-arwork').show();
 
      		setTimeout(function() {
      			$('.container-loading-artwork').hide();
      			$('.container-feeling-seven-sad').hide();
-     			$('.artwork-container-sad').show(); 
+     			$('#artwork-container-sad').fadeIn(3000);
      			$('#download-btn-container-sad').show();
-     		}, 3000); //loding artwork göms efter  6 sekunder och visar sedan konsverket
- 	
-     	}, 1000); // animationen visas i 1 sekund
-
+     		}, 3000); //loding artwork göms efter  3 sekunder och visar sedan konsverket
+     	}, 7000); // animationen visas i 1 sekund
 	});
 
 	$('#btn-feeling-eight-sad').on('click', function() {
@@ -158,20 +160,21 @@ var artworkLinkSad = document.createElement('a');
 		setTimeout(function() {
 			$('.container-feeling-eight-sad').hide()
      		$('.container-loading-artwork').show();
+     		//$('.text-rendering-arwork').show();
 
      		setTimeout(function() {
      			$('.container-loading-artwork').hide();
      			$('.container-feeling-eight-sad').hide();
-     			$('.artwork-container-sad').show(); 
+     			$('#artwork-container-sad').fadeIn(3000);
      			$('#download-btn-container-sad').show();
-     		}, 3000); //loding artwork göms efter  6 sekunder och visar sedan konsverket
- 	
-     	}, 1000); // animationen visas i 1 sekund
-
-
+     		}, 3000); //loding artwork göms efter  3 sekunder och visar sedan konsverket
+ 		}, 7000); // animationen visas i 1 sekund
 	});
 
-//////////////////////////////end sad btn flow start happy btn flow
+//////////////////////////////HAPPY FLOW//////////////////////////////////////////////
+
+
+
 
 	$('#happy-smiley-btn').on('click', function() {
 		
@@ -180,37 +183,36 @@ var artworkLinkSad = document.createElement('a');
 		$('#happy-smiley-btn').hide();
 		$('#sad-smiley-btn').hide();
 		$('body').addClass('happyBackground');
-		$('.how-do-you-feel').hide();
-		//$('#vertical-text-1').text("Happy Celebration").addClass('colorChangeHappyVerticalText');
-		$('.happy-copy-section-1').show();
+		$('.fly-in-text').hide();
+		$('#how-do-you-feel').hide();
+		$('.explanation-text').hide();
+		$('.happy-copy-section-1').fadeIn(2000);
 	});
 
 	$('#btn-feeling-one').on('click', function() {
+	
 		$('.container-feeling-one').show();
 		$('.button-section-1').hide();
-		$('.gif-section').hide();
 		
 		  setTimeout(function() {
      		$('.container-feeling-one').hide();
      		$('#btn-feeling-three').show();   
      		$('#btn-feeling-four').show();   
-     		$('.happy-copy-section-2').show();
-  		}, 1000);
-
+     		$('.happy-copy-section-2').fadeIn(2000);
+  		}, 7000);
 	});
 
 
 	$('#btn-feeling-two').on('click', function() {
 		$('.container-feeling-two').show();
-		$('.gif-section').hide();
 		$('.button-section-1').hide();
 
 		setTimeout(function() {
      		$('.container-feeling-two').hide();  
      		$('#btn-feeling-three').show();   
      		$('#btn-feeling-four').show(); 
-     		$('.happy-copy-section-2').show();  
-  		}, 1000);
+     		$('.happy-copy-section-2').fadeIn(2000);
+  		}, 7000);
 	});
 
 
@@ -222,8 +224,8 @@ var artworkLinkSad = document.createElement('a');
      		$('.container-feeling-three').hide();  
      		$('#btn-feeling-five').show();   
      		$('#btn-feeling-six').show();   
-     		$('.happy-copy-section-3').show();
-  		}, 1000);
+     		$('.happy-copy-section-3').fadeIn(2000);
+  		}, 7000);
 	});
 
 
@@ -235,8 +237,8 @@ var artworkLinkSad = document.createElement('a');
      		$('.container-feeling-four').hide();  
      		$('#btn-feeling-five').show();   
      		$('#btn-feeling-six').show();  
-     		$('.happy-copy-section-3').show(); 
-  		}, 1000);
+     		$('.happy-copy-section-3').fadeIn(2000);
+  		}, 7000);
 	});
 
 
@@ -248,8 +250,8 @@ var artworkLinkSad = document.createElement('a');
      		$('.container-feeling-five').hide();  
      		$('#btn-feeling-seven').show();   
      		$('#btn-feeling-eight').show();   
-     		$('.happy-copy-section-4').show(); 
-  		}, 1000);
+     		$('.happy-copy-section-4').fadeIn(2000);
+  		}, 7000);
 	});
 
 
@@ -261,23 +263,14 @@ var artworkLinkSad = document.createElement('a');
      		$('.container-feeling-six').hide();  
      		$('#btn-feeling-seven').show();   
      		$('#btn-feeling-eight').show();   
-     		$('.happy-copy-section-4').show(); 
-  		}, 1000);
-
+     		$('.happy-copy-section-4').fadeIn(2000);
+  		}, 7000);
 	});
 
 	$('#btn-feeling-seven').on('click', function() {
 		$('.button-section-4').hide();
 	  	$('.container-feeling-seven').show();
 
-/*
-gammal groll innan renderfunktionen
-		 setTimeout(function() {
-     		$('.container-feeling-seven').hide(); 
-     		$('.artwork-container-happy').show();  
-     		$('#download-btn-container-happy').show();
-  		}, 1000);
-*/
 		setTimeout(function() {
 			$('.container-feeling-seven').hide()
      		$('.container-loading-artwork').show();
@@ -285,13 +278,11 @@ gammal groll innan renderfunktionen
      		setTimeout(function() {
      			$('.container-loading-artwork').hide();
      			$('.container-feeling-seven').hide();
-     			$('.artwork-container-happy').show(); 
+     			$('#artwork-container-happy').fadeIn(3000);
      			$('#download-btn-container-happy').show();
      		}, 3000); //loding artwork göms efter  6 sekunder och visar sedan konsverket
  	
-     	}, 1000); // animationen visas i 1 sekund
-
-
+     	}, 7000); // animationen visas i 1 sekund
 	});
 
 	$('#btn-feeling-eight').on('click', function() {
@@ -301,21 +292,24 @@ gammal groll innan renderfunktionen
      	setTimeout(function() {
 			$('.container-feeling-eight').hide()
      		$('.container-loading-artwork').show();
+     		//$('.text-rendering-arwork').show();
 
      		setTimeout(function() {
      			$('.container-loading-artwork').hide();
      			$('.container-feeling-eight').hide();
-     			$('.artwork-container-happy').show(); 
+ 				$('#artwork-container-happy').fadeIn(3000);
      			$('#download-btn-container-happy').show();
      		}, 3000); //loding artwork göms efter  6 sekunder och visar sedan konsverket
- 	
-     	}, 1000); // animationen visas i 1 sekund
-  	});
+ 		}, 7000); // animationen visas i 1 sekund
+ 	});
 
 
-
-
-
-
-
+		/*
+			gammal groll innan renderfunktionen
+		 setTimeout(function() {
+     		$('.container-feeling-seven').hide(); 
+     		$('.artwork-container-happy').show();  
+     		$('#download-btn-container-happy').show();
+  		}, 1000);
+		*/
 });
